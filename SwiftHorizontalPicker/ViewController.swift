b1314be77fa83d5aee6af0665f7cbb3df4ea596f
Tor is Here <kittikorn.a@gmail.com>
2015-02-14 17:07:32 +0900

    normal vertical Picker

--- SwiftHorizontalPicker/ViewController.swift
+++ SwiftHorizontalPicker/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var myPickerView: UIPickerView!
+    
+    let pickerData:[String] = ["A", "B", "C", "D", "E", "F", "G"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.myPickerView.dataSource = self
+        self.myPickerView.delegate   = self
+        //flipPicker()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // UIPickerViewDataSource
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView,
+        rowHeightForComponent component: Int) -> CGFloat{
+        return 24.0
+    }
+    
+    // UIPickerViewDelegate
+    func pickerView(pickerView: UIPickerView,
+        numberOfRowsInComponent component: Int) -> Int {
+            return pickerData.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[row]
+    }
+    
+    func flipPicker() {
+        self.myPickerView.transform = CGAffineTransformRotate(self.myPickerView.transform, CGFloat(-M_PI/2))
+        self.myPickerView.reloadAllComponents()
+    }
 }
 
